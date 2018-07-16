c32c001732780745ccb51b9686d882e90ed01ef0
bill <bill.schrickel@gmail.com>
2018-07-15 21:25:44 -0500

    Initial commit again

--- app/src/main/java/com/bill/android/baking101/activities/MainActivity.java
+++ app/src/main/java/com/bill/android/baking101/activities/MainActivity.java
@@ -1,15 +1,47 @@
 package com.bill.android.baking101.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Movie;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.bill.android.baking101.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadRecipeData();
+    }
+
+    private void loadRecipeData() {
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if (isConnected) {
+            new FetchRecipeTask().execute();
+        }
+    }
+
+    public static class FetchRecipeTask extends AsyncTask<String, Void, ArrayList<Movie>> {
+
+
+        @Override
+        protected ArrayList<Movie> doInBackground(String... strings) {
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/bill/android/baking101/utils/NetworkUtils.java
@@ -0,0 +1,53 @@
+package com.bill.android.baking101.utils;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String RECIPE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
+    public static URL buildRecipeeUrl() {
+        URL url = null;
+
+        try {
+            url = new URL(RECIPE_URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(LOG_TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            Log.d(LOG_TAG, "A network error occurred!");
+            return null;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
